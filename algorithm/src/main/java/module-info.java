7c431bb7de69b1c983bd0dc1f46da4296a2a041f
Sebastian Schenker <github@asbestian.de>
2020-06-03 15:36:57 +0100

    Add Enumeration class

--- algorithm/src/main/java/module-info.java
+++ algorithm/src/main/java/module-info.java
@@ -1,10 +1,13 @@
 module algorithm {
+  exports de.asbestian.lotsizing.algorithm;
   exports de.asbestian.lotsizing.algorithm.scc;
   exports de.asbestian.lotsizing.algorithm.cycle;
 
+  opens de.asbestian.lotsizing.algorithm;
   opens de.asbestian.lotsizing.algorithm.scc;
   opens de.asbestian.lotsizing.algorithm.cycle;
 
+  requires input;
   requires graph;
   requires org.jgrapht.core;
   requires org.slf4j;
